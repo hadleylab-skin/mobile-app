169513ed240638cd39ac6f6df3c4fee8dd97fd81
Elena Grednikova <vesnushkalena@gmail.com>
2017-03-03 17:16:05 +0700

    Ref #25 Add back anatomical sites

---
+++ src/components/anatomical-site-widget/components/back/index.js
@@ -0,0 +1,20 @@
+import backHead from './images/body/back-head/back-head.png';
+import backNeck from './images/body/back-neck/back-neck.png';
+import upperRightBack from './images/body/upper-right-back/upper-right-back.png';
+import upperLeftBack from './images/body/upper-left-back/upper-left-back.png';
+
+import rightShoulderBack from './images/arms/right-shoulder-back/right-shoulder-back.png';
+import leftShoulderBack from './images/arms/left-shoulder-back/left-shoulder-back.png';
+import rightElbowBack from './images/arms/right-elbow-back/right-elbow-back.png';
+import leftElbowBack from './images/arms/left-elbow-back/left-elbow-back.png';
+import leftForearmBack from './images/arms/left-forearm-back/left-forearm-back.png';
+import rightForearmBack from './images/arms/right-forearm-back/right-forearm-back.png';
+import rightWristBack from './images/arms/right-wrist-back/right-wrist-back.png';
+import leftWristBack from './images/arms/left-wrist-back/left-wrist-back.png';
+import leftHandBack from './images/arms/left-hand-back/left-hand-back.png';
+import rightHandBack from './images/arms/right-hand-back/right-hand-back.png';
+
+
+export { backHead, backNeck, upperRightBack, upperLeftBack, rightShoulderBack,
+  leftShoulderBack, rightElbowBack, leftElbowBack, leftForearmBack, rightForearmBack,
+  rightWristBack, leftWristBack, leftHandBack, rightHandBack };
--- src/components/anatomical-site-widget/components/back/styles.js
+++ src/components/anatomical-site-widget/components/back/styles.js
@@ -1,12 +1,89 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container: {
-        position: 'absolute',
-        left: 0,
+    backHead: {
         top: 0,
-        right: 0,
-        width: 320,
-        height: 598,
+        left: 117,
+        width: 86,
+        height: 75,
+        justifyContent: 'flex-end',
+    },
+    backNeck: {
+        top: 76,
+        left: 142,
+        width: 35,
+        height: 34,
+    },
+    upperRightBack: {
+        top: 111,
+        left: 112,
+        width: 47,
+        height: 82,
+    },
+    upperLeftBack: {
+        top: 111,
+        right: 112,
+        width: 48,
+        height: 82,
+    },
+    rightShoulderBack: {
+        top: 92,
+        left: 84,
+        width: 60,
+        height: 67,
+    },
+    leftShoulderBack: {
+        top: 90,
+        right: 83,
+        width: 62,
+        height: 67,
+    },
+    rightElbowBack: {
+        top: 146,
+        left: 65,
+        width: 49,
+        height: 66,
+    },
+    leftElbowBack: {
+        top: 146,
+        right: 64,
+        width: 49,
+        height: 65,
+    },
+    rightForearmBack: {
+        top: 201,
+        left: 54,
+        width: 43,
+        height: 49,
+    },
+    leftForearmBack: {
+        top: 200,
+        right: 52,
+        width: 43,
+        height: 49,
+    },
+    rightWristBack: {
+        top: 239,
+        left: 43,
+        width: 40,
+        height: 48,
+    },
+    leftWristBack: {
+        top: 239,
+        right: 40,
+        width: 39,
+        height: 47,
+    },
+    rightHandBack: {
+        top: 279,
+        left: 14,
+        width: 61,
+        height: 75,
+    },
+    leftHandBack: {
+        top: 278,
+        right: 3,
+        width: 61,
+        height: 75,
     },
 });
