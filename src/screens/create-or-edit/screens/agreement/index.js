0fb16c1eae363a36525f0a07a8e4151d8ad0ef49
Ilya Beda <ir4y.ix@gmail.com>
2017-06-28 10:24:32 +0700

    Use MIT licence as consent text

--- src/screens/create-or-edit/screens/agreement/index.js
+++ src/screens/create-or-edit/screens/agreement/index.js
@@ -9,18 +9,31 @@ import backIcon from 'components/icons/back/back.png';
 import s from './styles';
 
 const HTML = `
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
+<h2>MIT License</h2>
+<p>
+Copyright (c) 2017 SkinIQ
+</p>
+<p>
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+</p>
+<p>
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+</p>
+<p>
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+</p>
 `;
 
 const AgreementScreen = React.createClass({
