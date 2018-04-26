d0782a2cfb2fcdf4dfe19ee6ef2d9e63f178e607
Alexandr Polev <aleksandr.p@pieoneers.com>
2018-04-26 13:13:10 +0700

    show default consent to sign if there were no consent files attached to the study

--- src/screens/participant-profile/invites/detail.js
+++ src/screens/participant-profile/invites/detail.js
@@ -1,11 +1,15 @@
 import _ from 'lodash';
 import React from 'react';
+import BaobabPropTypes from 'baobab-prop-types';
 import {
     View,
     Alert,
 } from 'react-native';
 import schema from 'libs/state';
+import { encryptRSA, decryptRSA } from 'services/keypair';
 import { InfoField, Button } from 'components';
+import { getSignatureRoute } from 'screens/signature';
+import { getAgreementRoute } from 'screens/create-or-edit/screens/agreement';
 import { getConsentDocsScreenRoute } from './consent-docs';
 
 import ss from './styles';
@@ -13,6 +17,7 @@ import ss from './styles';
 const model = {
     invites: {},
     declineInviteCursor: {},
+    consentCursor: {},
 };
 
 export const InviteDetailScreen = schema(model)(React.createClass({
@@ -22,9 +27,60 @@ export const InviteDetailScreen = schema(model)(React.createClass({
 
     contextTypes: {
         mainNavigator: React.PropTypes.object.isRequired, // eslint-disable-line
+        cursors: React.PropTypes.shape({
+            currentStudyPk: BaobabPropTypes.cursor.isRequired,
+        }),
         services: React.PropTypes.shape({
             declineInviteService: React.PropTypes.func.isRequired,
         }),
+    },
+
+    async onSign(signatureData) {
+        const patients = this.context.cursors.patients.get();
+        const patient = _.first(_.values(patients.data)).data;
+
+        await this.context.services.updatePatientConsentService(
+            patient.pk,
+            this.props.tree.consentCursor,
+            signatureData.encoded,
+        );
+        const consent = this.props.tree.consentCursor.get();
+        this.approveInvite(patient, consent.data.pk);
+    },
+
+    async approveInvite(patient, consentPk) {
+        // 1. restore original AES key of patient
+        const aesKey = await decryptRSA(patient.encryptedKey);
+        // 2. encrypt it with public key of invite's doctor and don't forget about coordinator
+        const { invite } = this.props;
+        const inviteDoctor = invite.doctor;
+        const data = {
+            consentPk,
+            doctorEncryptionKey: await encryptRSA(aesKey, inviteDoctor.publicKey),
+            ...inviteDoctor.coordinatorPublicKey && {
+                coordinatorEncryptionKey: await encryptRSA(aesKey, inviteDoctor.coordinatorPublicKey),
+            },
+        };
+        // 3. send it to server
+        const result = await this.context.services.approveInviteService(
+            invite.pk,
+            this.props.tree.approveInviteCursor,
+            data);
+        if (result.status === 'Succeed') {
+            if (this.props.tree.studies.data.get().length === 0) {
+                this.context.cursors.currentStudyPk.set('data', invite.study.pk);
+            }
+
+            const invites = _.filter(
+                this.props.tree.invites.data.get(),
+                (item) => item.pk !== invite.pk);
+
+            this.props.tree.invites.data.set(invites);
+            this.props.tree.studies.data.push(invite.study);
+            this.context.mainNavigator.popToTop();
+        } else {
+            Alert.alert('Error', JSON.stringify(result.error.data));
+        }
     },
 
     async declineInvite() {
@@ -64,14 +120,28 @@ export const InviteDetailScreen = schema(model)(React.createClass({
                         type="green"
                         title="Approve"
                         style={ss.button}
-                        onPress={() =>
-                            this.context.mainNavigator.push(
-                                getConsentDocsScreenRoute({
-                                    invite,
-                                    tree: this.props.tree,
-                                }, this.context)
-                            )
-                        }
+                        onPress={() => {
+                            if (_.isEmpty(invite.study.consentDocs)) {
+                                this.context.mainNavigator.push(
+                                    getAgreementRoute({
+                                        navigator: this.context.mainNavigator,
+                                        onAgree: () => this.context.mainNavigator.push(
+                                            getSignatureRoute({
+                                                navigator: this.context.mainNavigator,
+                                                onSave: this.onSign,
+                                            })),
+                                    })
+                                );
+                            } else {
+                                this.context.mainNavigator.push(
+                                    getConsentDocsScreenRoute({
+                                        invite,
+                                        tree: this.props.tree,
+                                        onSign: this.onSign,
+                                    }, this.context)
+                                );
+                            }
+                        }}
                     />
                     <Button
                         type="rect"
