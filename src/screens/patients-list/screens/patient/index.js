e1d134472f7ac6930d987278a08a3bade1a4f833
Alexandr Polev <aleksandr.p@pieoneers.com>
2018-06-06 12:41:14 +0700

    Remove obsolete import

--- src/screens/patients-list/screens/patient/index.js
+++ src/screens/patients-list/screens/patient/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import _ from 'lodash';
 import PropTypes from 'prop-types';
 import BaobabPropTypes from 'baobab-prop-types';
 import createReactClass from 'create-react-class';
