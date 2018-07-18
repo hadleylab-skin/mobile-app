f0a9931b3fe6777e2811116e369671c712300257
Elena <vesnushkalena@gmail.com>
2018-07-18 13:43:41 +0700

    Update crypto config screen styles

--- src/screens/crypto-config/styles.js
+++ src/screens/crypto-config/styles.js
@@ -1,8 +1,44 @@
 import { StyleSheet } from 'react-native';
 
+const borders = {
+    backgroundColor: '#fff',
+    borderTopColor: '#D1D1D6',
+    borderTopWidth: 0.5,
+    borderBottomColor: '#D1D1D6',
+    borderBottomWidth: 0.5,
+};
+
 export default StyleSheet.create({
-    container: { paddingTop: 10 },
-    group: { paddingBottom: 10 },
-    title: { fontSize: 30 },
-    mainTitle: { fontSize: 30, fontWeight: 'bold' },
+    container: {
+        flex: 1,
+        backgroundColor: '#fafafa',
+    },
+    content: {
+        paddingTop: 15,
+        paddingLeft: 45,
+        paddingRight: 45,
+        paddingBottom: 5,
+        marginBottom: 15,
+        ...borders,
+    },
+    fields: { ...borders },
+    title: {
+        fontSize: 30,
+        marginBottom: 10,
+    },
+    mainTitle: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        padding: 15,
+        paddingTop: 20,
+    },
+    text: {
+        paddingBottom: 15
+    },
+    button: {
+        marginTop: 15,
+        paddingLeft: 45,
+        paddingRight: 45,
+    },
 });
